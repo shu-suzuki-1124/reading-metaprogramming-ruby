12199e8bedba5522194330014db01d4a2febccfa
shu1124 <shu.suzuki1124@gmail.com>
2023-10-21 19:11:05 +0900

    04_block

--- 04_block/02_evil_mailbox.rb
+++ 04_block/02_evil_mailbox.rb
@@ -5,7 +5,7 @@
 # 2. EvilMailboxは、メールを送るメソッド `send_mail` を持ち、引数として宛先の文字列、本文の文字列を受け取る。結果の如何に関わらず、メソッドはnilをかえす。
 # 3. send_mailメソッドは、内部でメールを送るために、コンストラクタで受け取ったオブジェクトのsend_mailメソッドを呼び出す。このときのシグネチャは同じである。また、このメソッドはメールの送信が成功したか失敗したかをbool値で返す。
 # 4. EvilMailboxは、メールを受信するメソッド `receive_mail` を持つ
-# 5. receive_mailメソッドは、メールを受信するためにコンストラクタで受け取ったオブジェクトのreceive_mailメソッドを呼び出す。このオブジェクトのreceive_mailは、送信者と本文の2つの要素をもつ配列を返す。
+# 5. receive_mailメソッドは、メールを受信するためにコンストラクタで受け取ったオブジェクトのreceive_mailメソッドを呼び出す。このオブジェクトのreceive_mailは、送信者と本文の2つの要素をもつ配列で返す。
 # 6. receive_mailメソッドは、受け取ったメールを送信者と本文の2つの要素をもつ配列として返す
 #
 # 応用機能
@@ -17,3 +17,46 @@
 # 邪悪な機能
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
+
+class EvilMailbox
+  def initialize(mailbox, secret = nil)
+    @mailbox = mailbox
+    @mailbox.auth(secret) if secret
+
+    define_singleton_method :send_mail do |recipient, text, &block|
+      result = @mailbox.send_mail(recipient, text + secret.to_s)
+      block.call(result) if block
+      nil
+    end
+  end
+
+  def receive_mail
+    @mailbox.receive_mail
+  end
+end
+
+
+
+#↓テスト通らないver. 上のやつでいけた。
+# class EvilMailbox
+#   def initialize(mailbox, secret = nil)
+#     @mailbox = mailbox
+    
+#     unless secret.nil?
+#       @mailbox.auth(secret)
+#       @secret = secret
+#     end
+#   end
+
+#   def send_mail(recipient, text)
+#     text += @secret unless @secret.nil?
+
+#     result = @mailbox.send_mail(recipient, text)
+#     yield(result) if block_given?
+#     nil
+#   end
+
+#   def receive_mail
+#     @mailbox.receive_mail
+#   end
+# end
